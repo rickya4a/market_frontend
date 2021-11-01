257eff71ee66aefe543ab2d8a5123b0859ee8dc9
Ricky Aditya <rickyda79@gmail.com>
2021-11-01 10:08:20 +0700

    Toolbar update

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -4,7 +4,8 @@
       <h3 class="white--text">Hi, {{ username }}!</h3>
     </span>
     <span class="ml-2">
-      <h3 class="white--text">{{ score }} points</h3>
+      <h3 v-if="score" class="white--text">{{ score }} points</h3>
+      <h3 v-else class="white--text">You don't have any points yet</h3>
     </span>
   </v-system-bar>
 </template>
